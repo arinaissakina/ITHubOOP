d838690cebc0b03ed36f8f05d0105f020f719e05
Arina Issakina <arina.isakina@gmail.com>
2021-01-18 00:24:14 +0600

    third commit

---
+++ ResizableCircle.java
@@ -0,0 +1,22 @@
+public class ResizableCircle extends CircleObject implements Resizable {
+
+    public ResizableCircle() {
+        super();
+    }
+
+    public ResizableCircle(double radius) {
+        super(radius);
+    }
+
+    @Override
+    public String toString() {
+        return "ResizableCircle{" +
+                "radius=" + radius +
+                '}';
+    }
+
+    @Override
+    public void resize(int percent) {
+        radius += radius / 100 * percent;
+    }
+}
---
+++ TestResizableCircle.java
@@ -0,0 +1,11 @@
+public class TestResizableCircle {
+
+    public static void main(String[] args) {
+
+        ResizableCircle resizableCircle = new ResizableCircle();
+        System.out.println(resizableCircle);
+        resizableCircle.resize(50);
+        System.out.println(resizableCircle);
+
+    }
+}
